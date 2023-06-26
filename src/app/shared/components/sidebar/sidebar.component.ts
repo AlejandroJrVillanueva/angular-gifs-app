ee5cff7d6aff62a4409391fcc61b5d930f63dc8b
AlejandroJrVillanueva <Alejandrovillanuevareina@gmail.com>
2023-06-26 13:02:26 -0300

    add gifsService

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/service/gift.service';
 
 @Component({
   selector: 'shared-sidebar',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
 
+  constructor(private gifsService: GifsService){
+  }
+
+  get tags(): string[] {
+    //para no modificar el objeto directamente.
+    return this.gifsService.tagsHistory;
+  }
 }
