8d63262c3e12242a83d6ef1cb2e9f479676ac0a1
AlejandroJrVillanueva <Alejandrovillanuevareina@gmail.com>
2023-07-03 18:46:10 -0300

    add lazy-image for lazy load in my gifs

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NgModule } from '@angular/core';
 
+import { LazyImageComponent } from './components/lazy-image/lazy-image.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
+    LazyImageComponent,
     SidebarComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
+    LazyImageComponent,
     SidebarComponent
   ]
 })
