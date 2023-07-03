b2a15f97583f27060d078d6c65db5e127495e9ff
AlejandroJrVillanueva <Alejandrovillanuevareina@gmail.com>
2023-07-03 13:16:19 -0300

    add read localStorage from tagsHistory

--- src/app/gifs/service/gift.service.ts
+++ src/app/gifs/service/gift.service.ts
@@ -11,33 +11,33 @@ export class GifsService {
   private apiKey: string='7b8EXIgYlRMhh2JT4SFNuZ4pDoQvYIeL';
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs'
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadSaveStorage();
+    console.log('Gifs Service Ready!');
+  }
 
   get tagsHistory(){
     return [...this._tagsHistory];
   }
 
-   searchTag(tag:string):void {
-    if (tag.length === 0) return;
-    this.organizeHistory(tag);
+  searchTag(tag:string):void {
+  if (tag.length === 0) return;
+  this.organizeHistory(tag);
 
-    // forma tradicional de Js
-    // fetch('https://api.giphy.com/v1/gifs/search?api_key=7b8EXIgYlRMhh2JT4SFNuZ4pDoQvYIeL&q=flash&limit=10')
-    // .then(resp => resp.json())
-    // .then(data => console.log(data));
+  // forma tradicional de Js
+  // fetch('https://api.giphy.com/v1/gifs/search?api_key=7b8EXIgYlRMhh2JT4SFNuZ4pDoQvYIeL&q=flash&limit=10')
+  // .then(resp => resp.json())
+  // .then(data => console.log(data));
 
-    const params = new HttpParams()
-      .set('api_key', this.apiKey)
-      .set('q', tag)
-      .set('limit', '10')
+  const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('q', tag)
+    .set('limit', '10')
 
-    this.http.get<SearcResponse>(`${this.serviceUrl}/search`, {params})
-      .subscribe(resp => {
-        this.gifList = resp.data;
-        console.log({gifs: this.gifList});
-      });
-
-
+  this.http.get<SearcResponse>(`${this.serviceUrl}/search`, {params})
+    .subscribe(resp => {
+      this.gifList = resp.data;
+    });
   }
 
   private organizeHistory(tag: string){
@@ -55,6 +55,16 @@ export class GifsService {
   private saveLocalStorage():void
   {
     localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadSaveStorage():void{
+    if(!localStorage.getItem('history')) return;
+
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if(this._tagsHistory.length === 0) return
+
+    this.searchTag(this._tagsHistory[0]);
   }
 
 }
